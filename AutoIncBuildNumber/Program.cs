a13f9668228561285e790a0f8c7e42941d9ccf94
netcharm <netcharm@163.com>
2015-12-11 22:00:06 +0800

    Automatic increment the project building number

---
+++ AutoIncBuildNumber/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AutoIncBuildNumber
+{
+    class Program
+    {
+        private static string incBuildNo(string strVersion)
+        {
+            string[] version = strVersion.Split('.');
+            if ( version.Length == 4 )
+            {
+                try
+                {
+                    int buildNo = Convert.ToInt32(version[3]);
+                    version[3] = Convert.ToString( buildNo + 1 );
+                    return ( String.Join( ".", version ) );
+                }
+                catch
+                {
+                    return ( strVersion );
+                }
+            }
+            return ( strVersion );
+        }
+
+        static void Main( string[] args )
+        {
+            string strAssemblyVersion = "[assembly: AssemblyVersion( \"";
+            string strAssemblyFileVersion = "[assembly: AssemblyFileVersion( \"";
+            string strVersionEnd = "\" )]";
+
+            string[] lineSeparators = new string[] {"\n\r", "\r\n", "\r", "\n"};
+
+            string projectFolder = args[0].Trim(new char[] { '\"', ' '} );
+            string assemblyInfoSource = $@"{projectFolder}\Properties\AssemblyInfo.cs";
+
+            Console.WriteLine( assemblyInfoSource );
+            if ( File.Exists( assemblyInfoSource ) )
+            {
+                string assemblyInfo = File.ReadAllText( assemblyInfoSource, Encoding.UTF8 );
+                string[] lines = assemblyInfo.Split(lineSeparators, StringSplitOptions.None);
+
+                for ( int idx = 0; idx < lines.Length; idx++ )
+                {
+                    string line = lines[idx];
+                    if ( line.StartsWith( strAssemblyVersion ) &&
+                        line.EndsWith( strVersionEnd ) )
+                    {
+                        string strVersion = line.Substring( strAssemblyVersion.Length, line.Length - strAssemblyVersion.Length - strVersionEnd.Length );
+                        lines[idx] = line.Replace( strVersion, incBuildNo( strVersion ) );
+                    }
+                    else if ( line.StartsWith( strAssemblyFileVersion ) &&
+                        line.EndsWith( strVersionEnd ) )
+                    {
+                        string strVersion = line.Substring( strAssemblyFileVersion.Length, line.Length - strAssemblyFileVersion.Length - strVersionEnd.Length );
+                        lines[idx] = line.Replace( strVersion, incBuildNo( strVersion ) );
+                    }
+                }
+                if ( string.IsNullOrEmpty( lines[lines.Length - 1] ) )
+                {
+                    lines = string.Join( "\r", lines ).Trim().Split( lineSeparators, StringSplitOptions.None );
+                }
+                File.WriteAllLines( assemblyInfoSource, lines, Encoding.UTF8 );
+            }
+        }
+    }
+}
