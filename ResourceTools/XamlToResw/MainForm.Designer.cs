046f127d098e1aa94316af49ac706f1958dd6061
netcharm <netcharm@163.com>
2018-09-04 02:29:42 +0800

    Auto Extract UI String from project XAML files

---
+++ ResourceTools/XamlToResw/MainForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace XamlToResw
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.edDst = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // edDst
+            // 
+            this.edDst.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.edDst.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.edDst.Location = new System.Drawing.Point(13, 12);
+            this.edDst.Multiline = true;
+            this.edDst.Name = "edDst";
+            this.edDst.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.edDst.Size = new System.Drawing.Size(586, 412);
+            this.edDst.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 436);
+            this.Controls.Add(this.edDst);
+            this.Name = "MainForm";
+            this.Text = "Strings from XAML to Resw";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox edDst;
+    }
+}
+
---
+++ ResourceTools/XamlToResw/MainForm.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace XamlToResw
+{
+    public partial class MainForm : Form
+    {
+        private string[] ParseCommandLine(string cmdline)
+        {
+            List<string> args = new List<string>();
+
+            string[] cmds = cmdline.Split(new char[] { ' ' });
+            string arg = "";
+            foreach (string cmd in cmds)
+            {
+                if (cmd.StartsWith("\"") && cmd.EndsWith("\""))
+                {
+                    args.Add(cmd.Trim(new char[] { '\"', ' ' }));
+                    arg = "";
+                }
+                else if (cmd.StartsWith("\""))
+                {
+                    arg = cmd + " ";
+                }
+                else if (cmd.EndsWith("\""))
+                {
+                    arg += cmd;
+                    args.Add(arg.Trim(new char[] { '\"', ' ' }));
+                    arg = "";
+                }
+                else if (!string.IsNullOrEmpty(arg))
+                {
+                    arg += cmd + " ";
+                }
+                else
+                {
+                    if (!string.IsNullOrEmpty(cmd))
+                    {
+                        args.Add(cmd);
+                    }
+                    arg = "";
+                }
+#if DEBUG
+                Console.WriteLine($"Curent ARG: {cmd}, Parsed ARG: {arg}");
+#endif
+            }
+            return (args.GetRange(1, args.Count - 1).ToArray());
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string[] param = ParseCommandLine(Environment.CommandLine);
+            if (param.Length > 0)
+            {
+                var files = Directory.GetFiles(param[0], "*.xaml", SearchOption.AllDirectories);
+
+                StringBuilder sb = new StringBuilder();
+                foreach (var file in files)
+                {
+                    if (file.ToLower().Contains("\\obj\\")) continue;
+                    var lines = File.ReadAllLines(file, Encoding.UTF8);
+                    var resources = ParseXamlString(lines);
+                    //<data name="AppName" xml:space="preserve">
+                    //  <value>StringCodec</value>
+                    //</data>
+                    foreach (var kv in resources)
+                    {
+                        sb.AppendLine($"  <data name=\"{ kv.Key }\" xml:space=\"preserve\">");
+                        sb.AppendLine($"    <value>{ kv.Value.Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;") }</value>");
+                        sb.AppendLine($"  </data>");
+                    }
+                }
+                edDst.Text = string.Join("\n", sb);
+            }
+        }
+
+        private Dictionary<string, string> ParseXamlString(string[] lines)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            //System.Xml.XmlDocument xml = new System.Xml.XmlDocument();
+            //xml.LoadXml(string.Join("\n", lines));
+
+            using(var ms = new MemoryStream())
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(string.Join("", lines));
+                ms.Write(buffer, 0, buffer.Length);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                XDocument xmlx = XDocument.Load(ms);
+                XElement page = xmlx.Root;
+                //var all = xmlx.Root.DescendantNodes().OfType<XElement>();//.Select(x => x.Name).Distinct();
+
+                if (page != null)
+                {
+                    var childs = page.Elements();
+                    try
+                    {
+                        IEnumerable<XElement> elements = page.XPathSelectElements("//*");
+                        foreach (var element in elements)
+                        {
+                            if (element.HasAttributes)
+                            {
+                                var uid = string.Empty;
+                                var name = string.Empty;
+                                var header = string.Empty;
+                                var titlt = string.Empty;
+                                var label = string.Empty;
+                                var tooltip = string.Empty;
+                                var text = string.Empty;
+                                var placeholder = string.Empty;
+                                var content = string.Empty;
+                                var panetitle = string.Empty;
+
+                                Dictionary<string, string> attrs = new Dictionary<string, string>();
+                                foreach (var attr in element.Attributes())
+                                {
+                                    if (attr.Name.LocalName.Equals("Name", StringComparison.CurrentCultureIgnoreCase) &&
+                                       attr.Name.Namespace.NamespaceName.Equals("http://schemas.microsoft.com/winfx/2006/xaml", StringComparison.CurrentCultureIgnoreCase))
+                                    {
+                                        name = attr.Value;
+                                        if(string.IsNullOrEmpty(uid)) uid = attr.Value;
+                                    }
+                                    else if (attr.Name.LocalName.Equals("Uid", StringComparison.CurrentCultureIgnoreCase) &&
+                                       attr.Name.Namespace.NamespaceName.Equals("http://schemas.microsoft.com/winfx/2006/xaml", StringComparison.CurrentCultureIgnoreCase))
+                                    {
+                                        uid = attr.Value;
+                                    }
+                                    else if (attr.Name.LocalName.Equals("Header", StringComparison.CurrentCultureIgnoreCase))
+                                    {
+                                        attrs.Add($"{uid}.Header", attr.Value);
+                                    }
+                                    else if (attr.Name.LocalName.Equals("Title", StringComparison.CurrentCultureIgnoreCase))
+                                    {
+                                        attrs.Add($"{uid}.Title", attr.Value);
+                                    }
+                                    else if (attr.Name.LocalName.Equals("Label", StringComparison.CurrentCultureIgnoreCase))
+                                    {
+                                        attrs.Add($"{uid}.Label", attr.Value);
+                                    }
+                                    else if (attr.Name.LocalName.Equals("ToolTipService.ToolTip", StringComparison.CurrentCultureIgnoreCase))
+                                    {
+                                        attrs.Add($"{uid}.ToolTipService.ToolTip", attr.Value);
+                                    }
+                                    else if (attr.Name.LocalName.Equals("Text", StringComparison.CurrentCultureIgnoreCase))
+                                    {
+                                        attrs.Add($"{uid}.Text", attr.Value);
+                                    }
+                                    else if (attr.Name.LocalName.Equals("PlaceholderText", StringComparison.CurrentCultureIgnoreCase))
+                                    {
+                                        attrs.Add($"{uid}.PlaceholderText", attr.Value);
+                                    }
+                                    else if (attr.Name.LocalName.Equals("Content", StringComparison.CurrentCultureIgnoreCase))
+                                    {
+                                        attrs.Add($"{uid}.Content", attr.Value);
+                                    }
+                                    else if (attr.Name.LocalName.Equals("NavigationView.PaneTitle", StringComparison.CurrentCultureIgnoreCase))
+                                    {
+                                        attrs.Add($"{uid}.NavigationView.PaneTitle", attr.Value);
+                                    }
+                                }
+                                if (string.IsNullOrEmpty(uid) && !string.IsNullOrEmpty(name)) uid = name;
+                                if (string.IsNullOrEmpty(uid) && string.IsNullOrEmpty(name)) continue;
+
+                                result = new List<Dictionary<string, string>>() { result, attrs }.SelectMany(dict => dict).ToDictionary(pair => pair.Key, pair => pair.Value);
+                                //result = (Dictionary<string, string>)result.Concat(attrs);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+#if DEBUG
+                        System.Diagnostics.Debug.WriteLine($"{ex.Message}, {lines[0]}");
+#endif
+                        MessageBox.Show($"{ex.Message}, {lines[0]}", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                    }
+                }
+
+            }
+
+
+
+            return (result);
+        }
+    }
+}
