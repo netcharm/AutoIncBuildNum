a93780cc3195eaaf9b9bd23da398a1ddfea1a0a6
netcharm <netcharm@163.com>
2024-09-20 00:18:33 +0800

    Add: supported c# .net 8.0 project (maybe) Fixed; some minor bugs

--- AutoIncBuildNumber/Program.cs
+++ AutoIncBuildNumber/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace AutoIncBuildNumber
 {
@@ -17,29 +18,51 @@ namespace AutoIncBuildNumber
 
     class Program
     {
+        private static string PrettifyXML(XmlDocument doc)
+        {
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                OmitXmlDeclaration = true,
+                Encoding = Encoding.UTF8,
+                //CloseOutput = true,
+                Indent = true,
+                IndentChars = "  ",
+                NewLineChars = Environment.NewLine,
+                NewLineHandling = NewLineHandling.Replace
+            };
+            StringBuilder sb = new StringBuilder();
+            using (XmlWriter writer = XmlWriter.Create(sb, settings))
+            {
+                doc.Save(writer);
+            }
+            //text = Regex.Replace(text, $"</{tag}>", "</$1>" + Environment.NewLine + "    ");
+            //return sb.ToString().Replace("<?xml version=\"1.0\" encoding=\"utf-16\"?>", "").Trim();
+            return sb.ToString();
+        }
+
         /// <summary>
         /// Increment Revision Number
         /// </summary>
         /// <param name="strVersion"></param>
         /// <returns></returns>
-        private static string incBuildNo( string strVersion )
+        private static string incBuildNo(string strVersion)
         {
             string[] version = strVersion.Split('.');
 
-            if ( version.Length == 4 )
+            if (version.Length == 4)
             {
                 try
                 {
                     int buildNo = Convert.ToInt32(version[3]);
-                    version[3] = Convert.ToString( buildNo + 1 );
-                    return ( String.Join( ".", version ) );
+                    version[3] = Convert.ToString(buildNo + 1);
+                    return (String.Join(".", version));
                 }
                 catch
                 {
-                    return ( strVersion );
+                    return (strVersion);
                 }
             }
-            return ( strVersion );
+            return (strVersion);
         }
 
         /// <summary>
@@ -48,31 +71,31 @@ namespace AutoIncBuildNumber
         /// <param name="strVersion"></param>
         /// <param name="partnum"></param>
         /// <returns></returns>
-        private static string incBuildNo(string strVersion, VERSION_PART part = VERSION_PART.REVISION )
+        private static string incBuildNo(string strVersion, VERSION_PART part = VERSION_PART.REVISION)
         {
             string[] version = strVersion.Split('.');
             int version_idx = (int)part;
 
-            if ( version_idx >= version.Length ) version_idx = version.Length - 1;
-            if ( version_idx < 0 ) version_idx = 0;
+            if (version_idx >= version.Length) version_idx = version.Length - 1;
+            if (version_idx < 0) version_idx = 0;
 
             try
             {
-                if (version[version.Length-1].Trim().Equals("*", StringComparison.CurrentCultureIgnoreCase ))
+                if (version[version.Length - 1].Trim().Equals("*", StringComparison.CurrentCultureIgnoreCase))
                 {
                     version[version.Length - 1] = "0";
                 }
                 int buildNo = Convert.ToInt32(version[version_idx]);
-                version[version_idx] = Convert.ToString( buildNo + 1 );
+                version[version_idx] = Convert.ToString(buildNo + 1);
                 if (version_idx == 0) { version[1] = "0"; version[2] = "0"; version[3] = "0"; }
-                else if(version_idx == 1) { version[2] = "0"; version[3] = "0"; }
+                else if (version_idx == 1) { version[2] = "0"; version[3] = "0"; }
                 else if (version_idx == 2) { version[3] = "0"; }
-                Console.WriteLine( $"Version From: [{ strVersion }] To: [{string.Join( ".", version)}]" );
-                return ( string.Join( ".", version ) );
+                Console.WriteLine($"Version From: [{strVersion}] To: [{string.Join(".", version)}]");
+                return (string.Join(".", version));
             }
             catch
             {
-                return ( strVersion );
+                return (strVersion);
             }
         }
 
@@ -82,21 +105,21 @@ namespace AutoIncBuildNumber
 
             string[] cmds = cmdline.Split( new char[] { ' ' } );
             string arg = "";
-            foreach ( string cmd in cmds )
+            foreach (string cmd in cmds)
             {
-                if ( cmd.StartsWith( "\"" ) && cmd.EndsWith( "\"" ) )
+                if (cmd.StartsWith("\"") && cmd.EndsWith("\""))
                 {
-                    args.Add( cmd.Trim( new char[] { '\"', ' ' } ) );
+                    args.Add(cmd.Trim(new char[] { '\"', ' ' }));
                     arg = "";
                 }
-                else if( cmd.StartsWith( "\"" ) )
+                else if (cmd.StartsWith("\""))
                 {
                     arg = cmd + " ";
                 }
-                else if ( cmd.EndsWith( "\"" ) )
+                else if (cmd.EndsWith("\""))
                 {
                     arg += cmd;
-                    args.Add( arg.Trim( new char[] { '\"', ' ' } ) );
+                    args.Add(arg.Trim(new char[] { '\"', ' ' }));
                     arg = "";
                 }
                 else if (!string.IsNullOrEmpty(arg))
@@ -105,20 +128,20 @@ namespace AutoIncBuildNumber
                 }
                 else
                 {
-                    if ( !string.IsNullOrEmpty( cmd ) )
+                    if (!string.IsNullOrEmpty(cmd))
                     {
-                        args.Add( cmd );
+                        args.Add(cmd);
                     }
                     arg = "";
                 }
 #if DEBUG
                 Console.WriteLine( $"Curent ARG: {cmd}, Parsed ARG: {arg}" );
 #endif
-            }            
-            return ( args.GetRange( 1, args.Count - 1 ).ToArray() );
+            }
+            return (args.GetRange(1, args.Count - 1).ToArray());
         }
 
-        static void Main( string[] args )
+        static void Main(string[] args)
         {
             string[] param = ParseCommandLine(Environment.CommandLine);
 #if DEBUG
@@ -126,14 +149,9 @@ namespace AutoIncBuildNumber
             //Console.WriteLine( string.Join( " : ", Environment.GetCommandLineArgs() ) );
             Console.WriteLine( string.Join( " : ", param ) );
 #endif
-            string strAssemblyVersion = "[assembly: AssemblyVersion( \"";
-            string strAssemblyFileVersion = "[assembly: AssemblyFileVersion( \"";
-            string strVersionEnd = "\" )]";
-            Regex pattenAssemblyVersion = new Regex( @"^\[assembly:( )*?Assembly(File)*?Version\(( )*?""(\d+)\.(\d+)\.(\d+)\.(\d+)""( )*?\)( )*?\].*?$" );
-
             string[] lineSeparators = new string[] {"\n\r", "\r\n", "\r", "\n"};
 
-            if( param.Length < 1)
+            if (param.Length < 1)
             {
                 return;
             }
@@ -144,11 +162,11 @@ namespace AutoIncBuildNumber
 #endif
 
             VERSION_PART versionPart = VERSION_PART.REVISION;
-            if ( param.Length >= 2 )
+            if (param.Length >= 2)
             {
                 try
                 {
-                    versionPart = (VERSION_PART) Enum.Parse( typeof( VERSION_PART ), param[1].Trim(), true );
+                    versionPart = (VERSION_PART)Enum.Parse(typeof(VERSION_PART), param[1].Trim(), true);
                     // if using .net framework > v4.0 can using following codes
                     //if (Enum.TryParse( param[1].Trim(), true, out versionPart ))
                     //{
@@ -163,50 +181,132 @@ namespace AutoIncBuildNumber
             Console.WriteLine( projectFolder );
             Console.WriteLine( versionPart );
 #endif
-            List<string> assemblyInfoSource = new List<string>();
-            assemblyInfoSource.Add( $@"{projectFolder}\Properties\AssemblyInfo.cs" );
-            assemblyInfoSource.Add( $@"{projectFolder}\AssemblyInfo.cs" );
-
-            foreach ( string assemblyInfoFile in assemblyInfoSource )
+            List<string> assemblyInfoSource = new List<string>
             {
-                if ( File.Exists( assemblyInfoFile ) )
-                {
+                $@"{projectFolder}\Properties\AssemblyInfo.cs",
+                $@"{projectFolder}\AssemblyInfo.cs",
+                $@"{projectFolder}\*.csproj"
+            };
+
+            string strAssemblyVersion = "[assembly: AssemblyVersion( \"";
+            string strAssemblyFileVersion = "[assembly: AssemblyFileVersion( \"";
+            string strVersionEnd = "\" )]";
+            Regex pattenAssemblyVersion = new Regex( @"^\[assembly:( )*?Assembly(File)*?Version\(( )*?""(\d+)\.(\d+)\.(\d+)\.(\d+)""( )*?\)( )*?\].*?$" );
+
+            bool changed = false;
+
+            foreach (string assemblyInfoFile in assemblyInfoSource)
+            {
+                var ext = Path.GetExtension(assemblyInfoFile).ToLower();
 #if DEBUG
                     Console.WriteLine( assemblyInfoFile );
 #endif
+                if (ext.Equals(".cs") && File.Exists(assemblyInfoFile))
+                {
+                    string assemblyInfo = File.ReadAllText(assemblyInfoFile, Encoding.UTF8);
+                    if (assemblyInfo.Contains("[assembly: ThemeInfo(")) continue;
 
-                    string assemblyInfo = File.ReadAllText( assemblyInfoFile, Encoding.UTF8 );
                     string[] lines = assemblyInfo.Split(lineSeparators, StringSplitOptions.None);
 
-                    for ( int idx = 0; idx < lines.Length; idx++ )
+                    for (int idx = 0; idx < lines.Length; idx++)
                     {
                         string line = lines[idx];
                         Match mo = pattenAssemblyVersion.Match(line);
 
-                        if ( line.StartsWith( strAssemblyVersion ) &&
-                            line.EndsWith( strVersionEnd ) )
+                        if (line.StartsWith(strAssemblyVersion) && line.EndsWith(strVersionEnd))
                         {
-                            string strVersion = line.Substring( strAssemblyVersion.Length, line.Length - strAssemblyVersion.Length - strVersionEnd.Length );
-                            lines[idx] = line.Replace( strVersion, incBuildNo( strVersion, versionPart ) );
+                            string strVersion = line.Substring(strAssemblyVersion.Length, line.Length - strAssemblyVersion.Length - strVersionEnd.Length);
+                            lines[idx] = line.Replace(strVersion, incBuildNo(strVersion, versionPart));
+                            changed = true;
                         }
-                        else if ( line.StartsWith( strAssemblyFileVersion ) &&
-                            line.EndsWith( strVersionEnd ) )
+                        else if (line.StartsWith(strAssemblyFileVersion) && line.EndsWith(strVersionEnd))
                         {
-                            string strVersion = line.Substring( strAssemblyFileVersion.Length, line.Length - strAssemblyFileVersion.Length - strVersionEnd.Length );
-                            lines[idx] = line.Replace( strVersion, incBuildNo( strVersion, versionPart ) );
+                            string strVersion = line.Substring(strAssemblyFileVersion.Length, line.Length - strAssemblyFileVersion.Length - strVersionEnd.Length);
+                            lines[idx] = line.Replace(strVersion, incBuildNo(strVersion, versionPart));
+                            changed = true;
                         }
-                        else if( mo.Length >0)
+                        else if (mo.Length > 0)
                         {
                             string strVersion = $"{mo.Groups[4]}.{mo.Groups[5]}.{mo.Groups[6]}.{mo.Groups[7]}";
-                            lines[idx] = line.Replace( strVersion, incBuildNo( strVersion, versionPart ) );
+                            lines[idx] = line.Replace(strVersion, incBuildNo(strVersion, versionPart));
+                            changed = true;
                         }
-
                     }
-                    if ( string.IsNullOrEmpty( lines[lines.Length - 1] ) )
+                    if (string.IsNullOrEmpty(lines[lines.Length - 1]))
                     {
-                        lines = string.Join( "\r", lines ).Trim().Split( lineSeparators, StringSplitOptions.None );
+                        lines = string.Join("\r", lines).Trim().Split(lineSeparators, StringSplitOptions.None);
+                        changed = true;
                     }
-                    File.WriteAllLines( assemblyInfoFile, lines, Encoding.UTF8 );
+                    if (changed)
+                    {
+                        File.WriteAllLines(assemblyInfoFile, lines, Encoding.UTF8);
+                        break;
+                    }
+                }
+                else if (ext.Equals(".csproj"))
+                {
+                    var csprojs = Directory.GetFiles(projectFolder, "*.csproj", SearchOption.TopDirectoryOnly);
+                    if (csprojs.Length > 0)
+                    {
+                        var version_tags = new string[]{ "Version", "AssemblyVersion", "FileVersion" };
+                        var csproj = csprojs[0];
+                        var xml = new XmlDocument();
+                        xml.PreserveWhitespace = true;
+                        xml.Load(csproj);
+
+                        var targets = xml.GetElementsByTagName("TargetFramework");
+                        if (targets.Count > 0)
+                        {
+                            var root = targets[0].ParentNode;
+                            foreach (var tag in version_tags)
+                            {
+                                var tags = xml.GetElementsByTagName(tag);
+                                if (tags.Count > 0 && tags[0].ParentNode == root)
+                                {
+                                    var ver_tag = tags[0];
+                                    if (ver_tag.Value != null)
+                                    {
+                                        ver_tag.Value = incBuildNo(ver_tag.Value, versionPart);
+                                        changed = true;
+                                    }
+                                    else if (ver_tag.InnerText != null)
+                                    {
+                                        ver_tag.InnerText = incBuildNo(ver_tag.InnerText, versionPart);
+                                        changed = true;
+                                    }
+                                }
+                                else
+                                {
+                                    var ver_tag = xml.CreateNode(XmlNodeType.Element, tag, xml.NamespaceURI);
+                                    ver_tag.InnerText = "1.0.0.0";
+                                    root.AppendChild(ver_tag);
+                                    root.AppendChild(xml.CreateTextNode(Environment.NewLine));
+                                    changed = true;
+                                }
+                            }
+                        }
+                        if (changed)
+                        {
+                            var lines = PrettifyXML(xml).Split(lineSeparators, StringSplitOptions.None);
+                            for (var i = 0; i< lines.Length; i++)
+                            {
+                                var line = lines[i];
+                                foreach (var tag in version_tags)
+                                {
+                                    if (Regex.IsMatch(line, $"<{tag}>(.+)</{tag}>", RegexOptions.IgnoreCase))
+                                    {
+                                        lines[i] = "    " + line.Trim();
+                                    }
+                                }
+                                if (Regex.IsMatch(line, @"</?PropertyGroup>", RegexOptions.IgnoreCase))
+                                {
+                                    lines[i] = "  " + line.Trim();
+                                }
+                            }
+                            File.WriteAllText(csproj, string.Join(Environment.NewLine, lines), Encoding.UTF8);
+                            break;
+                        }
+                    }
                 }
             }
         }
